1ba8f7fe07f5f99fa2f293f6bf32a22eaf6bb070
hfdzafrnsyh <hafidzafrnsyh@outlook.co.id>
2023-04-14 22:36:06 +0700

    update for deploy

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const app = require('./app');
+const serverless = require("serverless-http");
 
 const PORT = process.env.PORT || 5000;
 
@@ -6,3 +7,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server run in ${PORT}`);
 })
+
+module.exports.handler = serverless(app)
