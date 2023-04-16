ca4bccf90fece2fbb33627e6fa00be0500a83fe6
Hafidz Afriansyah <62727044+hfdzafrnsyh@users.noreply.github.com>
2023-04-16 12:04:26 +0700

    update db.js
    update db.js

--- database/db.js
+++ database/db.js
@@ -5,9 +5,9 @@ const URI = process.env.URI;
 // const DB_NAME = process.env.DB_NAME;
 
 
-main().catch(err => console.log(err));
-
 async function main() {
     await mongoose.connect(`${URI}`);
     console.log("Database Connection Success")
 }
+
+main().catch(err => console.log(err));
