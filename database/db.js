6bdc72af02366464f0db84f14fb165ad100fb14e
hfdzafrnsyh <hafidzafrnsyh@outlook.co.id>
2023-04-16 15:42:52 +0700

    update db

--- database/db.js
+++ database/db.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const URI = process.env.URI;
 // const DB_NAME = process.env.DB_NAME;
 
-<<<<<<< HEAD
 async function main() {
     await mongoose.connect(`${URI}`);
     console.log("Database Connection Success")
@@ -12,17 +11,3 @@ async function main() {
 
 
 main().catch(err => console.log(err));
-=======
-
-
-async function main() {
-    await mongoose.connect(`${URI}`);
-    console.log("Database Connection Success")
-    
-}
-
-
-
-main().catch(err => console.log(err));
-
->>>>>>> 33d05c8035f776a1a2e78d6b286868faa8c023d0
