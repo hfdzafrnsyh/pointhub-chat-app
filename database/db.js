db47ce51ceebe3aa1969195d56dced38664fef2e
Hafidz Afriansyah <62727044+hfdzafrnsyh@users.noreply.github.com>
2023-04-16 13:09:15 +0700

    update db
    update db

--- database/db.js
+++ database/db.js
@@ -4,10 +4,21 @@ const mongoose = require('mongoose');
 const URI = process.env.URI;
 // const DB_NAME = process.env.DB_NAME;
 
+const schema = new mongoose.Schema({
+  name: String
+});
+
 
 async function main() {
     await mongoose.connect(`${URI}`);
     console.log("Database Connection Success")
+    
+    mongoose.model('User', schema);
+
+    await mongoose.model('User').findOne(); // Works!
 }
 
+
+
 main().catch(err => console.log(err));
+
