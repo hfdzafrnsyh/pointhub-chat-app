33d05c8035f776a1a2e78d6b286868faa8c023d0
Hafidz Afriansyah <62727044+hfdzafrnsyh@users.noreply.github.com>
2023-04-16 13:13:48 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -4,18 +4,12 @@ const mongoose = require('mongoose');
 const URI = process.env.URI;
 // const DB_NAME = process.env.DB_NAME;
 
-const schema = new mongoose.Schema({
-  name: String
-});
 
 
 async function main() {
     await mongoose.connect(`${URI}`);
     console.log("Database Connection Success")
     
-    mongoose.model('Invoices', schema);
-
-    await mongoose.model('Invoices').findOne(); // Works!
 }
 
 
